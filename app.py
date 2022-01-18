a88d2887d4610b1b7ce6444c04ea64063979605b
Suroj Bera <surojbera@gmail.com>
2022-01-19 00:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return '''<h1 style='color: blue;'>Grettings from Suroj!! Welcome you all!!</h1>'''
+  return '''<html>
+<head>
+<style>
+h1 {text-align: center; color:blue; margin-top:100px;}
+h2 {text-align: center;color:green;}
+div {text-align: center;}
+</style>
+</head>
+<body>
+<h1>Grettings from Suroj!!</h1>
+<h2>Welcome you all to Dockerized Python application!!</h2>
+</body>
+</html>'''
 
-@app.route("/dashboard")
-def dashboard():
-  #return '''<h1 style='color: green;'>This is about us</h1>'''
-  return render_template("dashboard.html")
+@app.route("/admin")
+def admin():
+  return '''<h1 style='color: green;'>This is about us</h1>'''
   
 if __name__ == "__main__":
   port =  int(os.environ.get("PORT",8081))
